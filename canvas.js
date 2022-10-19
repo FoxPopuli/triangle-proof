bcba9af8aece00105b1e59daa4ca66e7d66b4550
Taylor Casse <taylorcasse@gmail.com>
2022-10-19 23:04:07 +0200

    Added second canvas layer and hitting enter toggles it on and off. Holes method doesn't work, can't click through second layer

--- canvas.js
+++ canvas.js
@@ -1,12 +1,25 @@
+const canvasHeight = window.innerHeight;
+const canvasWidth = window.innerWidth;
+
+
 
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
-canvas.height = window.innerHeight;
-canvas.width = window.innerWidth;
+canvas.height = canvasHeight;
+canvas.width = canvasWidth;
+
+const canvas2 = document.querySelector('#canvas2');
+const ctx2 = canvas2.getContext('2d');
+canvas2.height = canvasHeight;
+canvas2.width = canvasWidth;
 
 // get two angles for each node
 // start angle is the smaller one
-
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        canvas2.classList.toggle('hidden')
+    }
+})
 
 let vertices = [
     {x: 100, y: 100, r: 5}, 
@@ -20,56 +33,32 @@ let startY;
 let currentShape;
 
 function draw (shapeArr) {
-    // Clear the canvas
+    // Clear the canvases
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx2.clearRect(0, 0, canvas2.width, canvas2.height);
 
-    // Draw lines
-    const lineAngles = [];
-    for (let i in shapeArr) {
+    // Draw top layer
+    ctx2.beginPath();
+    ctx2.moveTo(0, 0);
+    ctx2.lineTo(canvas2.width, 0);
+    ctx2.lineTo(canvas2.width, canvas2.height);
+    ctx2.lineTo(0, canvas2.height);
+    ctx2.closePath();
+    // ctx2.fillStyle = 'green';
+    // ctx2.fill();
+    ctx2.stroke();
 
-        let j;
-        if (+i === 2) {
-            j = 0;
-        } else {
-            j = +i + 1;
-        }
+    // Draw triangle 
+    ctx.beginPath();
+    ctx.moveTo(vertices[0].x, vertices[0].y);
+    ctx.lineTo(vertices[1].x, vertices[1].y);
+    ctx.lineTo(vertices[2].x, vertices[2].y);
+    ctx.closePath();
+    ctx.stroke();
 
-
-        let k;
-        if (j === 2) {
-            k = 0;
-        } else {
-            k = j + 1;
-        }
-
-        let x1 = shapeArr[i].x;
-        let y1 = shapeArr[i].y;
-
-        let x2 = shapeArr[j].x;
-        let y2 = shapeArr[j].y;
-
-        let x3 = shapeArr[k].x;
-        let y3 = shapeArr[k].y;
-
-        ctx.beginPath();
-        ctx.moveTo(x1, y1);
-        ctx.lineTo(x2, y2);
-        ctx.stroke();
-
-
-        i++;
-    }
-
-    let j = 2;
-    for (let i in lineAngles) {
-        let j = i + 1
-        if (j === 3) {j = 0};
-
-        ctx.beginPath();
-        let rad = 5;
-        ctx.arc(shapeArr[i].x, shapeArr[i].y, rad, );
-
-    }
+    // Draw anti-triangle
+    // ctx2.begin
+    
 
 
     // Draw circles
@@ -82,18 +71,13 @@ function draw (shapeArr) {
         ctx.closePath();
     })
 
-
-    // get angles
-    for (let i in shapeArr) {
-        let j;
-        if (+i === 2) {
-            j = 0;
-        } else {
-            j = +i + 1;
-        }
-
-
-    }
+    // Draw anti-circles
+    shapeArr.forEach(vertex => {
+        ctx2.beginPath();
+        ctx2.arc(vertex.x, vertex.y, vertex.r, 0, Math.PI*2, true);
+        ctx2.stroke();
+        // ctx2.closePath();
+    })
 
 }
 draw(vertices);
